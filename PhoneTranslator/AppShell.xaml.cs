3e57922ee28c1b9493d27d876d40392fb4f3cda6
4m <4m@DESKTOP-LNGDBA8>
2024-09-11 10:16:52 +0200

    Dodaj pliki projektów.

---
+++ PhoneTranslator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneTranslator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PhoneTranslator/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace PhoneTranslator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string translatedNumber;
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = CoreAnimation.PhonewordTranslator.ToNumber(enteredNumber);
+        }
+
+    }
+}
